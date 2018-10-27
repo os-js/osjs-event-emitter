b5470da4a49787e767af67fa0fee0bdba40a486f
Anders Evenrud <andersevenrud@gmail.com>
2018-10-28 00:13:43 +0200

    Added node as target

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@ const mode = process.env.NODE_ENV || 'development';
 module.exports = {
   mode,
   devtool: 'source-map',
+  target: 'node',
   entry: [
     path.resolve(__dirname, 'index.js'),
   ],
